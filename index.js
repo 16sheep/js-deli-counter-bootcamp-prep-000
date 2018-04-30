343cbecf7464a0867d27eac15500743940081c18
marjutubli@gmail.com <marjutubli@gmail.com>
2018-04-30 21:49:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 //Accepts existing line of people(array) and name of the new person(string) as parameters and returns a welcoming string with the new persons place in the line.
 
+let counter = 0;
+
 const takeANumber = (katzDeliLine, newPerson) => {
-  katzDeliLine.push(newPerson);
-  return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`;
+  katzDeliLine.push(newPerson)
+  
+  counter++; 
+  
+  return `Welcome. You are number ${counter} in line.`;
 };
 
 
